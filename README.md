760574db5cb59d2dbae1692e63e4b29ca15cd819
Peter Kneale <peterkneale@gmail.com>
2018-04-23 21:39:04 +1000

    Setup ci for autofac extras

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Envoy.Extras.Autofac
-Autofac extensions for Envoy
+Autofac extensions for [Envoy](https://github.com/peterkneale/envoy)
+
+## Installation
+
+via Package Manager
+
+```Install-Package Envoy.Extras.Autofac```
+
+via .NET CLI: 
+    
+```dotnet add package Envoy.Extras.Autofac```
+
+via Packet CLI
+
+```paket add Envoy.Extras.Autofac```
+
+## Using Envoy with Autofac Container Integration
+...
---
+++ src/Envoy.Extras.Autofac/EnvoyAutofacModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace Envoy.Extras.Autofac
+{
+    public class EnvoyAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EnvoyAutofacResolver>().As<IResolver>().SingleInstance();
+            builder.RegisterType<Dispatcher>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<Executor>().AsImplementedInterfaces().InstancePerLifetimeScope();
+        }
+    }
+}
